113ae63f666fa98e6573afaf02040d6d8dc1a074
Manu88x <emmanuelokoth112@gmail.com>
2024-11-14 21:56:12 +0300

    ive set up the structure created clock header and footer along with db.json which i will use for the external API

---
+++ src/Components/App.jsx
@@ -0,0 +1,18 @@
+import Header from './header.jsx';
+import Footer from './Footer.jsx';
+
+import Clock from './Clock.jsx';
+
+
+function App() {      
+  return (
+   <>
+   <Header/>
+    <Clock/>
+    <Footer/>
+   </>
+  );
+}
+
+export default App;
+
